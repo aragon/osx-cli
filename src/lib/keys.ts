5f5390ec26caecbf99c8fdcbafd619a9e58161d2
pythonpete32 <aaron@blackscale.io>
2023-06-14 22:48:06 +0400

    style: fix spelling

--- src/lib/keys.ts
+++ src/lib/keys.ts
@@ -10,13 +10,19 @@ const accountTenderlyKey = 'tenderly-key';
 export const privateKeySchema = z
   .string()
   .length(64, 'Private key must be exactly 64 characters long.')
-  .regex(/^[a-fA-F0-9]*$/, 'Private key must only contain hexadecimal characters.');
+  .regex(
+    /^[a-fA-F0-9]*$/,
+    'Private key must only contain hexadecimal characters.',
+  );
 
 // Define the schema for the Tenderly key
 export const tenderlyKeySchema = z
   .string()
   .length(32, 'Tenderly key must be exactly 32 characters long.')
-  .regex(/^[a-zA-Z0-9]*$/, 'Tenderly key must only contain numbers and letters.');
+  .regex(
+    /^[a-zA-Z0-9]*$/,
+    'Tenderly key must only contain numbers and letters.',
+  );
 
 export const setPrivateKey = async (privateKey: string): Promise<void> => {
   try {
@@ -34,7 +40,7 @@ export const setTenderlyKey = async (tenderlyKey: string): Promise<void> => {
   try {
     tenderlyKeySchema.parse(tenderlyKey);
     await keytar.setPassword(service, accountTenderlyKey, tenderlyKey);
-    console.log(chalk.green('Tenderly Keys stored successfully.'));
+    console.log(chalk.green('Tenderly Key stored successfully.'));
   } catch (error) {
     error instanceof z.ZodError
       ? console.error('Invalid input:', error.errors)
