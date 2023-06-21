682907378b5434c205065adf937c72dffbcfddb2
pythonpete32 <aaron@blackscale.io>
2023-06-21 15:50:35 +0400

    docs: rename to

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { Command } from 'commander';
 import { settings } from './commands/settings/index.js';
 
 const program = new Command();
-program.description('AragonOSx Manager');
+program.description('OSx DevKit');
 program.version('0.0.0');
 program.addCommand(settings);
 
